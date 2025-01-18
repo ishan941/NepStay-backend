d97af5bbfa55d753cdebc565c352e5d2828333e8
Ishan Shrestha <iahan.esan18@gmail.com>
2025-01-18 19:37:55 +0545

    backend for Nepstay

---
+++ djangobnb_backend/django_backend/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/properties/', include('property.urls')),
+#     path('api/auth/', include('useraccount.urls')),
+#     path('api/chat/', include('chat.urls')),
+] 
---
+++ djangobnb_backend/property/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from .models import Property
+
+# from useraccount.serializers import UserDetailSerializer
+
+
+class PropertiesListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Property
+        fields = (
+            'id',
+            'title',
+            'price_per_night',
+            'image_url',
+        )
+
